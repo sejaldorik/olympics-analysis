8cdf10fd46fd5fac8924aee0c7385b544825acc4
Sejal Dorik <sejaldorik23@gmail.com>
2026-04-10 23:32:18 +0530

    Initial commit

---
+++ preprocessor.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+def preprocess(df,region_df):
+
+
+
+    # filter for summer olympics
+    df = df[df['Season'] == 'Summer']
+
+    # merge with region_df
+    df = df.merge(region_df, on='NOC', how='left')
+
+    # remove duplicates
+    df.drop_duplicates(inplace=True)
+
+    # one hot encoding
+    df = pd.concat([df, pd.get_dummies(df['Medal'])], axis=1)
+
+    return df
